f186d7f8ee0cdc499d514760894fae7607e27991
John Firebaugh <john.firebaugh@gmail.com>
2012-01-08 17:27:23 -0800

    Unshare and expand Complex#inspect specs
    It behaves completely differently between 1.8 and 1.9 so no point
    in having a shared file.

--- spec/ruby/core/complex/inspect_spec.rb
+++ spec/ruby/core/complex/inspect_spec.rb
@@ -1,7 +1,48 @@
-require File.expand_path('../../../shared/complex/inspect', __FILE__)
+require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is "1.9" do
   describe "Complex#inspect" do
-    it_behaves_like(:complex_inspect, :inspect)
+    it "returns (${real}+${image}i) for positive imaginary parts" do
+      Complex(1).inspect.should == "(1+0i)"
+      Complex(7).inspect.should == "(7+0i)"
+      Complex(-1, 4).inspect.should == "(-1+4i)"
+      Complex(-7, 6.7).inspect.should == "(-7+6.7i)"
+    end
+
+    it "returns (${real}-${image}i) for negative imaginary parts" do
+      Complex(0, -1).inspect.should == "(0-1i)"
+      Complex(-1, -4).inspect.should == "(-1-4i)"
+      Complex(-7, -6.7).inspect.should == "(-7-6.7i)"
+    end
+
+    it "returns (1+0.0i) for Complex(1, 0.0)" do
+      Complex(1, 0.0).inspect.should == "(1+0.0i)"
+    end
+
+    it "returns (1-0.0i) for Complex(1, -0.0)" do
+      Complex(1, -0.0).inspect.should == "(1-0.0i)"
+    end
+
+    it "returns (1+Infinity*i) for Complex(1, Infinity)" do
+      Complex(1, infinity_value).inspect.should == "(1+Infinity*i)"
+    end
+
+    it "returns (1-Infinity*i) for Complex(1, -Infinity)" do
+      Complex(1, -infinity_value).inspect.should == "(1-Infinity*i)"
+    end
+
+    it "returns (1+NaN*i) for Complex(1, NaN)" do
+      Complex(1, nan_value).inspect.should == "(1+NaN*i)"
+    end
+
+    it "sends #inspect to real and imaginary parts" do
+      real = mock_numeric('real')
+      imag = mock_numeric('imag')
+      real.should_receive(:inspect).and_return("r")
+      imag.should_receive(:inspect).and_return("i")
+      imag.should_receive(:<).with(0).and_return(false)
+      imag.should_receive(:abs).and_return(imag)
+      Complex(real, imag).inspect.should == "(r+i*i)"
+    end
   end
 end
--- spec/ruby/library/complex/inspect_spec.rb
+++ spec/ruby/library/complex/inspect_spec.rb
@@ -1,10 +1,18 @@
-require File.expand_path('../../../shared/complex/inspect', __FILE__)
+require File.expand_path('../../../spec_helper', __FILE__)
 
 ruby_version_is ""..."1.9" do
-
   require 'complex'
 
   describe "Complex#inspect" do
-    it_behaves_like(:complex_inspect, :inspect)
+    it "returns Complex(real, image)" do
+      # Guard against the Mathn library
+      conflicts_with :Prime do
+        Complex(1).inspect.should == "Complex(1, 0)"
+        Complex(7).inspect.should == "Complex(7, 0)"
+      end
+
+      Complex(-1, 4).inspect.should == "Complex(-1, 4)"
+      Complex(-7, 6.7).inspect.should == "Complex(-7, 6.7)"
+    end
   end
 end
